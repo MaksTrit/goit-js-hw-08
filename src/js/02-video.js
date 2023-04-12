f78daec5f127bffc4945a6027f30b95471af4ceb
Maksym Triteinikov <i.max.trit@gmail.com>
2023-04-12 14:11:48 +0300

    hw8 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = "videoplayer-current-time";
+let parsedCurrentTime = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) ?? 0;
+
+player.on('timeupdate', throttle((data) => {
+    if (data.percent < 1) {
+        localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));       
+    }
+        
+    else {
+        player.off('timeupdate');
+        localStorage.clear();
+    }
+}, 1000));
+       
+player.setCurrentTime(parsedCurrentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+
+const userData = {};
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+fillText();
+
+form.addEventListener("input", throttle(()=> saveData(), 500));
+
+form.addEventListener("submit", clearData);
+
+function saveData() {    
+    userData.email = form.elements.email.value.trim();
+    userData.message = form.elements.message.value.trim();
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));    
+}
+
+function clearData(evt) {
+    evt.preventDefault();
+    console.log(userData);
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset();
+    
+}
+
+function fillText() {
+    const storedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {};
+    form.elements.email.value = storedData.email ?? "";
+    form.elements.message.value = storedData.message ?? "";
+}
+
+
 
